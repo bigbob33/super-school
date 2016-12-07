3393d04ff4441ba3cf60ad7f8be4b51cfd058514
Michal Kosyk <gager33@gmail.com>
2016-12-07 13:59:19 +0100

    Added get subjects to ReportsController

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,8 @@
 class ReportsController < ApplicationController
   before_filter :authenticate_user!
   expose(:subject_items) { SubjectItem.includes(:teacher, :students) }
+
+  def subjects
+    @subjects = subject_items
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :students do
     get :subjects
   end
+
+  get "reports/subjects"
 end
