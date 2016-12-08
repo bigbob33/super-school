32d4ff54d3c46d0c1ce2ec308c9dfd6a0209f9d4
Michal Kosyk <gager33@gmail.com>
2016-12-08 02:14:17 +0100

    Added Devise's validatable module to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :rememberable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 end
